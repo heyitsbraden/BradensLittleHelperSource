c7fd78a7ecc2cf57a2372ffb572df1d3d7c5e9d7
heyitsbraden_ <100321821+heyitsbraden@users.noreply.github.com>
2022-03-16 16:24:44 -0700

    Deleted invite.js because it was useless lmao

--- Commands/invite.js
+++
@@ -1,7 +0,0 @@
-exports.run = async (bot, message, args) => {
-    message.channel.send("You can't invite this specific bot, but you can invite a slightly modified version of it called Little Helper! INVITE: https://discord.com/api/oauth2/authorize?client_id=926142873500536863&permissions=8&scope=bot");
-}
-
-exports.help = {
-    name:"invite"
-}
